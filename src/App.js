464ff509b4d8b69e6b29fff2fdc781fc4267ab5f
unknown <suren.rayamajhi1425@gmail.com>
2023-12-20 02:13:44 +0545

    create header components and using useLocation, useNaviage for navigating the header pages

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Offers from "./pages/Offers";
 import ForgotPassword from "./pages/ForgotPassword";
+import Header from "./components/Header";
 function App() {
   return (
     <>
      <Router>
+      <Header/>
       <Routes>
         <Route  path="/" element={<Home/>} />
         <Route  path="/profile" element={<Profile />} />
